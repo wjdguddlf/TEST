95285278fcda5b5652ddd8c0764a4cbb85162017
wjdguddlf <hihi@gaeasoft.co.kr>
2020-10-16 17:59:07 +0900

    첫번째

--- javaProject/src/javaProject/tes.java
+++ javaProject/src/javaProject/tes.java
@@ -3,15 +3,8 @@ package javaProject;
 public class tes {
 	
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("첫번째");
 		
-<<<<<<< main
-=======
-		System.out.println("두번째");
-		
->>>>>>> d41ef9a 세번째
 	}
-
 }
