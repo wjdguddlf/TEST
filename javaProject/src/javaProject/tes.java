ef6e1b963efece025af0720cd24c436563ee780d
wjdguddlf <hihi@gaeasoft.co.kr>
2020-10-16 17:39:55 +0900

    init

--- javaProject/src/javaProject/tes.java
+++ javaProject/src/javaProject/tes.java
@@ -4,19 +4,7 @@ public class tes {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("tet");
 		
-		System.out.println("member");
-		
-		System.out.println("member222");
-		
-		System.out.println("member222_test");
-		
-		
-		System.out.println("test commit");
-		
-		
-		System.out.println("되돌리기!!");
 	}
 
 }
